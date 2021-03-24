7110b3f80961fdfae859c9d8eabafeef2dfd92e3
heagan01 <heagan866@gmail.com>
2021-03-24 23:39:14 +0700

    Added some new rendering things

--- src/App.js
+++ src/App.js
@@ -39,9 +39,11 @@ import "./App.css";
 // }
 
 let price = 0
+var nf = new Intl.NumberFormat();
 
 const data250ml = [
     { color: "brown", name: "Americano", id: "americano", price: 18000 },
+    { color: "grey", name: "Cookies n Cream", id: "cookie", price: 18000 },
     { color: "brown", name: "Aren Latte", id: "aren", price: 18000 },
     { color: "grey", name: "Vanilla Latte", id: "vanilla", price: 18000 },
     { color: "green", name: "Pandan Latte", id: "pandan", price: 18000 },
@@ -58,8 +60,8 @@ const data250ml = [
 
 const data500ml = [
   { color: "brown", name: "Americano", id: "americano", price: 35000 },
+    { color: "grey", name: "Cookies n Cream", id: "cookie", price: 35000 },
   { color: "brown", name: "Aren Latte", id: "aren", price: 35000 },
-  { color: "grey", name: "Vanilla Latte", id: "vanilla", price: 35000 },
   { color: "green", name: "Pandan Latte", id: "pandan", price: 35000 },
   { color: "purple", name: "Taro Latte", id: "taro", price: 35000 },
   { color: "brown", name: "Rum Coffee", id: "rum", price: 35000 },
@@ -101,6 +103,7 @@ class SignUpDialog extends React.Component {
   constructor(eprops) {
     super(eprops);
     this.calculate = this.calculateR.bind(this);
+    this.price = 0;
     // This may be the most inefficient code I've ever written
     this.addItem = this.addItem.bind(this);
     this.state = {
@@ -144,41 +147,59 @@ class SignUpDialog extends React.Component {
       <div className="products">
 
           <div className='gray item'><h1>Discount</h1><input className='input250ml' id='Discount' onChange={this.addItem} placeholder='Discount'></input></div>
-          
+
           {data250ml.map(item => (
               <Item color={item.color} name={item.name} add_item={this.addItem}/>
           ))}
 
       </div>,
       <div className="preview">
-        {this.state.americano != 0 && "Americano: " + this.state.americano}
-        {this.state.americano != 0 && <br />}
-        {this.state.aren != 0 && "Aren Latte: " + this.state.aren}
-        {this.state.aren != 0 && <br />}
-        {this.state.vanilla != 0 && "Vanilla Latte: " + this.state.vanilla}
-        {this.state.vanilla != 0 && <br />}
-        {this.state.pandan != 0 && "Pandan Latte: " + this.state.pandan}
-        {this.state.pandan != 0 && <br />}
-        {this.state.taroL != 0 && "Taro Latte: " + this.state.taroL}
-        {this.state.taroL != 0 && <br />}
-        {this.state.rum != 0 && "Rum Coffee: " + this.state.rum}
-        {this.state.rum != 0 && <br />}
-        {this.state.dolce != 0 && "Dolce Coffee: " + this.state.dolce}
-        {this.state.dolce != 0 && <br />}
-        {this.state.mocha != 0 && "Mochacino: " + this.state.mocha}
-        {this.state.mocha != 0 && <br />}
-        {this.state.matcha != 0 && "Creamy Matcha: " + this.state.matcha}
-        {this.state.matcha != 0 && <br />}
-        {this.state.cookie != 0 && "Cookies N Cream: " + this.state.cookie}
-        {this.state.cookie != 0 && <br />}
-        {this.state.taro != 0 && "Taro Frappé: " + this.state.taro}
-        {this.state.taro != 0 && <br />}
-        {this.state.choco != 0 && "Choco Signature: " + this.state.choco}
-        {this.state.choco != 0 && <br />}
-        {this.state.milktea != 0 && "Brown Sugar Milk Tea: " + this.state.milktea}
-        {this.state.milktea != 0 && <br />}
-        {this.state.mango != 0 && "Mango Tea: " + this.state.mango}
-        {this.state.mango != 0 && <br />}
+        ----------------------<br/>
+     <pre>     Selera Ngopi</pre>
+        ----------------------<br/>
+        <pre>{this.state.americano != 0 &&  this.state.americano + "x  Americano"}</pre>
+        <pre>{this.state.americano != 0 && "250ml          " + nf.format(this.state.americano * 18000)}</pre>
+        <pre>{this.state.americano != 0 && <br/>}</pre>
+        <pre>{this.state.aren != 0 && this.state.aren + "x  Aren Latte"}</pre>
+        <pre>{this.state.aren != 0 && "250ml          " + nf.format(this.state.aren * 18000)}</pre>
+        <pre>{this.state.aren != 0 && <br />}</pre>
+        <pre>{this.state.vanilla != 0 && this.state.vanilla + "x  Vanilla Latte"}</pre>
+        <pre>{this.state.vanilla != 0 && "250ml          " + nf.format(this.state.vanilla * 18000)}</pre>
+        <pre>{this.state.vanilla != 0 && <br />}</pre>
+        <pre>{this.state.pandan != 0 && this.state.pandan + "x  Pandan Latte"}</pre>
+        <pre>{this.state.pandan != 0 && "250ml          " + nf.format(this.state.pandan * 18000)}</pre>
+        <pre>{this.state.pandan != 0 && <br />}</pre>
+        <pre>{this.state.taroL != 0 && this.state.taroL + "x  Taro Latte"}</pre>
+        <pre>{this.state.taroL != 0 && "250ml          " + nf.format(this.state.taroL * 18000)}</pre>
+        <pre>{this.state.taroL != 0 && <br />}</pre>
+        <pre>{this.state.rum != 0 && this.state.rum + "x  Rum Coffee"}</pre>
+        <pre>{this.state.rum != 0 && "250ml          " + nf.format(this.state.rum * 18000)}</pre>
+        <pre>{this.state.rum != 0 && <br />}</pre>
+        <pre>{this.state.dolce != 0 && this.state.dolce + "x  Dolce Coffee"}</pre>
+        <pre>{this.state.dolce != 0 && "250ml          " + nf.format(this.state.dolce * 18000)}</pre>
+        <pre>{this.state.dolce != 0 && <br />}</pre>
+        <pre>{this.state.mocha != 0 && this.state.mocha + "x  Mochacino"}</pre>
+        <pre>{this.state.mocha != 0 && "250ml          " + nf.format(this.state.mocha*18000)}</pre>
+        <pre>{this.state.mocha != 0 && <br />}</pre>
+        <pre>{this.state.matcha != 0 && this.state.matcha + "x  Creamy Matcha"}</pre>
+        <pre>{this.state.matcha != 0 && "250ml          " + nf.format(this.state.matcha*18000)}</pre>
+        <pre>{this.state.matcha != 0 && <br />}</pre>
+        <pre>{this.state.cookie != 0 && this.state.cookie + "x  Cookies & Cream"}</pre>
+        <pre>{this.state.cookie != 0 && "250ml          " + nf.format(this.state.cookie * 18000)}</pre>
+        <pre>{this.state.cookie != 0 && <br />}</pre>
+        <pre>{this.state.taro != 0 && this.state.taro + "x  Taro Frappé"}</pre>
+        <pre>{this.state.taro != 0 && "250ml          " + nf.format(this.state.taro * 18000)}</pre>
+        <pre>{this.state.taro != 0 && <br />}</pre>
+        <pre>{this.state.choco != 0 && this.state.choco + "x  Choco Signature"}</pre>
+        <pre>{this.state.choco != 0 && "250ml          " + nf.format(this.state.choco * 18000)}</pre>
+        <pre>{this.state.choco != 0 && <br />}</pre>
+        <pre>{this.state.milktea != 0 && this.state.milktea + "x  Brown Sugar Milktea"}</pre>
+        <pre>{this.state.milktea != 0 && "250ml           18.000" + nf.format(this.state.milktea * 18000)}</pre>
+        <pre>{this.state.milktea != 0 && <br />}</pre>
+        <pre>{this.state.mango != 0 && this.state.mango + "x  Mango Tea"}</pre>
+        <pre>{this.state.mango != 0 && "250ml           18.000" + nf.format(this.state.mango * 18000)}</pre>
+        <pre>{this.state.mango != 0 && <br />}</pre>
+        <pre>{this.state != 0 && "TOTAL	        " + this.price}</pre>
       </div>,
       <svg viewBox="0 0 200 200" className='blob' xmlns="http://www.w3.org/2000/svg">
         <path className='path' fill="#FF0066" d="M36.3,-52.6C45.5,-43.3,50.3,-30.6,56.7,-17C63.1,-3.4,71,11.1,69.8,25C68.5,38.9,58,52.1,44.8,56.4C31.6,60.6,15.8,56,2.1,53.1C-11.6,50.1,-23.1,48.9,-37.2,44.9C-51.2,40.9,-67.8,34.2,-73.9,22.4C-80.1,10.5,-75.7,-6.3,-69.8,-22C-63.8,-37.7,-56.1,-52.2,-44.2,-60.7C-32.3,-69.1,-16.2,-71.5,-1.3,-69.7C13.5,-67.9,27.1,-61.9,36.3,-52.6Z" transform="translate(100 100)" />
@@ -187,8 +208,9 @@ class SignUpDialog extends React.Component {
   }
 
   // this is the function to calculate all of the prices
-  calculateR(e) {
-    price = this.state.americano * data250ml[0].price + this.state.aren * data250ml[0].price + this.state.vanilla * data250ml[0].price + this.state.pandan * data250ml[0].price + this.state.taroL * data250ml[0].price + this.state.rum * data250ml[0].price + this.state.dolce * data250ml[0].price + this.state.mocha * data250ml[0].price + this.state.matcha * data250ml[0].price + this.state.cookie * data250ml[0].price + this.state.taro * data250ml[0].price + this.state.choco * data250ml[0].price + this.state.milktea * data250ml[0].price + this.state.mango * data250ml[12].price + this.state500.americano * data500ml[0].price + this.state500.aren * data500ml[0].price + this.state500.vanilla * data500ml[0].price + this.state500.pandan * data500ml[0].price + this.state500.taroL * data500ml[0].price + this.state500.rum * data500ml[0].price + this.state500.dolce * data500ml[0].price + this.state500.mocha * data500ml[0].price + this.state500.matcha * data500ml[0].price + this.state500.cookie * data500ml[0].price + this.state500.taro * data500ml[0].price + this.state500.choco * data500ml[0].price + this.state500.milktea * data500ml[0].price + this.state500.mango * data500ml[12].price - this.state.discount
+  calculateR(e){
+    this.price = this.state.americano * data250ml[0].price + this.state.aren * data250ml[0].price + this.state.vanilla * data250ml[0].price + this.state.pandan * data250ml[0].price + this.state.taroL * data250ml[0].price + this.state.rum * data250ml[0].price + this.state.dolce * data250ml[0].price + this.state.mocha * data250ml[0].price + this.state.matcha * data250ml[0].price + this.state.cookie * data250ml[0].price + this.state.taro * data250ml[0].price + this.state.choco * data250ml[0].price + this.state.milktea * data250ml[0].price + this.state.mango * data250ml[12].price + this.state500.americano * data500ml[0].price + this.state500.aren * data500ml[0].price + this.state500.vanilla * data500ml[0].price + this.state500.pandan * data500ml[0].price + this.state500.taroL * data500ml[0].price + this.state500.rum * data500ml[0].price + this.state500.dolce * data500ml[0].price + this.state500.mocha * data500ml[0].price + this.state500.matcha * data500ml[0].price + this.state500.cookie * data500ml[0].price + this.state500.taro * data500ml[0].price + this.state500.choco * data500ml[0].price + this.state500.milktea * data500ml[0].price + this.state500.mango * data500ml[12].price - this.state.discount
+
     console.log(data500ml[12].price)
     console.log(price)
   }
